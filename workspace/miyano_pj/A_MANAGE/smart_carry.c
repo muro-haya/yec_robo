be3ad3bcebbb472ac1608cfb483bd1d1f3614ea8
muro-haya <haya0908to@yahoo.co.jp>
2024-08-24 10:59:56 +0900

    スマートキャリー攻略　カメラ以外作成完了

--- workspace/miyano_pj/A_MANAGE/smart_carry.c
+++ workspace/miyano_pj/A_MANAGE/smart_carry.c
@@ -17,25 +17,35 @@
 #include "smart_carry.h"
 
 /* 適合値 */
-uint16_t x_u16_smart_carry_deg = 100;       /* スマートキャリー指定角[deg] */
+uint16_t x_u16_smart_carry_3_distance = 50;         /* キャリーボトル認識距離[mm] */
+uint16_t x_u16_smart_carry_4_movement = 70;         /* ティポジション通過移動量[mm] */
+uint16_t x_u16_smart_carry_5_movement = 150;        /* ディセイブルゾーン通過移動量[mm] */
+uint16_t x_u16_smart_carry_6_movement = 300;        /* ランディング移動量[mm] */
+uint16_t x_u16_smart_carry_7_movement = 150;        /* 後進移動量[mm] */
+uint16_t x_u16_smart_carry_8_deg      = 70;         /* 指定旋回角[deg] */
+uint16_t x_u16_smart_carry_4_spd      = 70;         /* ティポジション通過速速 */
+uint16_t x_u16_smart_carry_6_spd      = 300;        /* ランディング速度 */
+uint16_t x_u16_smart_carry_7_spd      = 150;        /* 後進速度 */
+uint16_t x_u16_smart_carry_deg        = 100;        /* スマートキャリー指定角[deg] */
+
 
 /* 外部公開変数 */
-uint16_t g_u16_smart_carry_phase;           /* スマートキャリーフェイズカウント */
+uint16_t g_u16_smart_carry_phase;                   /* スマートキャリーフェイズカウント */
 
 /* 外部非公開変数 */
-uint16_t reset_flg;                         /* リセットフラグ */
+uint16_t reset_flg;                                 /* リセットフラグ */
 
 /* 外部非公開関数 */
-void turn_jdg_color( void );                /* 色認識まで旋回するフェイズ */
-void turn_jdg_deg( void );                  /* 指定角まで旋回するフェイズ */
-void rd_jdg_color( void );                  /* 色認識までR・D走行するフェイズ */
-void rd_jdg_movement( void );               /* 指定距離までR・D走行するフェイズ */
-void line_jdg_obj( void );                  /* 物体付近までライントレースするフェイズ */
-void line_jdg_movement( void );             /* 指定距離までライントレースするフェイズ */
+void turn_jdg_color( uint16_t fin_color );                          /* 色認識まで旋回するフェイズ */
+void turn_jdg_deg( uint16_t fin_deg );                              /* 指定角まで旋回するフェイズ */
+void rd_jdg_color( uint16_t fin_color );                            /* 色認識までR・D走行するフェイズ */
+void rd_jdg_movement( uint16_t fin_movement, int16_t run_spd );     /* 指定距離までR・D走行するフェイズ */
+void line_jdg_obj( uint16_t fin_distance );                         /* 物体付近までライントレースするフェイズ */
+void line_jdg_movement( uint16_t fin_movement );                    /* 指定距離までライントレースするフェイズ */
 
 /* スマートキャリー初期化処理 */
 void ini_smart_carry( void ){
-    g_u16_smart_carry_phase = 0;
+    g_u16_smart_carry_phase = 1;
     reset_flg = 0;
 }
 
@@ -47,25 +57,34 @@ bool_t cyc_smart_carry( void ){
 
     switch (g_u16_smart_carry_phase)
     {
-    case TURN_JDG_COLOR:
-        turn_jdg_color();
+    case 1:
+        turn_jdg_color( RESULT_RED );
         break;
-    case TURN_JDG_DEG:
-        turn_jdg_deg();
+    case 2:
+        rd_jdg_color( RESULT_BLUE );
         break;
-    case RD_JDG_COLOR:
-        rd_jdg_color();
+    case 3:
+        line_jdg_obj( x_u16_smart_carry_3_distance );
         break;
-    case RD_JDG_MOVEMENT:
-        rd_jdg_movement();
+    case 4:
+        rd_jdg_movement( x_u16_smart_carry_4_movement, x_u16_smart_carry_4_spd );
         break;
-    case LINE_JDG_OBJ:
-        line_jdg_obj();
+    case 5:
+        line_jdg_movement( x_u16_smart_carry_5_movement );
         break;
-    case LINE_JDG_MOVEMENT:
-        line_jdg_movement();
+    case 6:
+        rd_jdg_movement( x_u16_smart_carry_6_movement, x_u16_smart_carry_6_spd );
         break;
-    case SMART_CARRY_END:
+    case 7:
+        rd_jdg_movement( x_u16_smart_carry_7_movement, x_u16_smart_carry_7_spd );
+        break;
+    case 8:
+        turn_jdg_deg( x_u16_smart_carry_8_deg );
+        break;
+    case 9:
+        rd_jdg_color(RESULT_BLACK);
+        break;
+    case 10:
         bdat = 1;
         break;
     default:
@@ -76,20 +95,20 @@ bool_t cyc_smart_carry( void ){
 }
 
 /* 色認識まで旋回するフェイズ */
-void turn_jdg_color( void ){
+void turn_jdg_color( uint16_t fin_color ){
     uint16_t color_result;
 
     g_u16_ctl_main_mode = CONST_TURN;
     g_u16_const_run_way = 1;
     g_s16_const_run_spd = 100;
 
-    /* カメラで色認識 */
+    /* fin_color == カメラで色認識結果 */
     if( 1 ){
         g_u16_smart_carry_phase += 1;
     }
 }
 /* 指定角まで旋回するフェイズ */
-void turn_jdg_deg( void ){
+void turn_jdg_deg( uint16_t fin_deg ){
     uint16_t deg_result;
 
     if( 0 == reset_flg ){
@@ -102,14 +121,14 @@ void turn_jdg_deg( void ){
         g_s16_const_run_spd = 100;
 
         deg_result = get_cal_movement_body_deg();
-        if( x_u16_smart_carry_deg < deg_result ){
+        if( fin_deg < deg_result ){
             g_u16_smart_carry_phase += 1;
             reset_flg = 0;
         }
     }
 }
 /* 色認識までR・D走行するフェイズ */
-void rd_jdg_color( void ){
+void rd_jdg_color( uint16_t fin_color ){
     uint16_t color_result;
 
     g_u16_ctl_main_mode = CONST_RUN;
@@ -117,12 +136,12 @@ void rd_jdg_color( void ){
     g_s16_const_run_spd = 200;
 
     color_result = get_rec_color();
-    if( 1 ){
+    if( fin_color == color_result ){
         g_u16_smart_carry_phase += 1;
     }
 }
 /* 指定距離までR・D走行するフェイズ */
-void rd_jdg_movement( void ){
+void rd_jdg_movement( uint16_t fin_movement, int16_t run_spd ){
     uint16_t movement_result;
 
     if( 0 == reset_flg ){
@@ -135,14 +154,14 @@ void rd_jdg_movement( void ){
         g_s16_const_run_spd = 200;
         
         movement_result = get_cal_movement();
-        if( 1 ){
+        if( fin_movement < movement_result ){
             g_u16_smart_carry_phase += 1;
             reset_flg = 0;
         }
     }
 }
 /* 物体付近までライントレースするフェイズ */
-void line_jdg_obj( void ){
+void line_jdg_obj( uint16_t fin_distance ){
     uint16_t distance_result;
 
     g_u16_ctl_main_mode = LINETRACE_RUN;
@@ -150,12 +169,12 @@ void line_jdg_obj( void ){
     g_u16_linetrace_run_bsV = 100;
 
     distance_result = get_cal_distance();
-    if( 1 ){
+    if( fin_distance < distance_result ){
         g_u16_smart_carry_phase += 1;
     }
 }
 /* 指定距離までライントレースするフェイズ */
-void line_jdg_movement( void ){
+void line_jdg_movement( uint16_t fin_movement ){
     uint16_t movement_result;
 
     if( 0 == reset_flg ){
@@ -168,7 +187,7 @@ void line_jdg_movement( void ){
         g_u16_linetrace_run_bsV = 100;
         
         movement_result = get_cal_movement();
-        if( 1 ){
+        if( fin_movement < movement_result ){
             g_u16_smart_carry_phase += 1;
             reset_flg = 0;
         }
--- workspace/miyano_pj/A_MANAGE/smart_carry.h
+++ workspace/miyano_pj/A_MANAGE/smart_carry.h
@@ -1,11 +1,4 @@
 /* スマートキャリー攻略 */
-#define TURN_JDG_COLOR      0               /* 色認識まで旋回するフェイズ */
-#define TURN_JDG_DEG        1               /* 指定角まで旋回するフェイズ */
-#define RD_JDG_COLOR        2               /* 色認識までR・D走行するフェイズ */
-#define RD_JDG_MOVEMENT     3               /* 指定距離までR・D走行するフェイズ */
-#define LINE_JDG_OBJ        4               /* 物体付近までライントレースするフェイズ */
-#define LINE_JDG_MOVEMENT   5               /* 指定距離までライントレースするフェイズ */
-#define SMART_CARRY_END     6               /* 終わり */
 
 /* 適合値 */
 extern uint16_t x_u16_smart_carry_deg;      /* スマートキャリー指定角[deg] */
