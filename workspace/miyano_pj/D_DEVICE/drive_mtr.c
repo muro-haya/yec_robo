b5423b6d1f0d0d0f2d71ef78e82b1b9630c5c8a6
eaglraune <miyano1996@yahoo.co.jp>
2024-09-28 10:20:10 +0900

    回転数制御実装（Pのみ）

--- workspace/miyano_pj/D_DEVICE/drive_mtr.c
+++ workspace/miyano_pj/D_DEVICE/drive_mtr.c
@@ -11,6 +11,8 @@
 #include "kernel_cfg.h"
 #include "syssvc/serial.h"
 
+#include "../M_CTL/const_run.h"
+
 #include "drive_mtr.h"
 
 #define MOTOR_CNT_BUF 20
@@ -21,47 +23,38 @@ pup_motor_t *motorR;
 
 /*  適合値*/
 uint16_t x_u16_drive_mtr_rpmL_kp = 10;
-uint16_t x_u16_drive_mtr_rpmL_ki = 5;
-uint16_t x_u16_drive_mtr_rpmL_kd = 5;
+uint16_t x_u16_drive_mtr_rpmL_kd = 0;
+uint16_t x_u16_drive_mtr_rpmL_ki = 0;
 
-uint16_t x_u16_drive_mtr_rpmR_kp = 10;//46
-uint16_t x_u16_drive_mtr_rpmR_ki = 0;//52
+uint16_t x_u16_drive_mtr_rpmR_kp = 11;//46
 uint16_t x_u16_drive_mtr_rpmR_kd = 0;//52
+uint16_t x_u16_drive_mtr_rpmR_ki = 0;//52
 
 /* 外部公開変数 */
 int16_t g_s16_drive_mtr_spdL;
 int16_t g_s16_drive_mtr_spdR;
 
 /* 外部非公開変数 */
-int16_t s16_drive_cntL_buf[MOTOR_CNT_BUF];
-int16_t s16_drive_cntR_buf[MOTOR_CNT_BUF];
-uint16_t u16_drive_cntR_buf_index;
-uint16_t u16_drive_cntL_buf_re_flg;
-uint16_t u16_drive_cntR_buf_re_flg;
-
 int16_t s16_drive_rpmL_p;
 int16_t s16_drive_rpmL_i;
 int16_t s16_drive_rpmL_d;
-int16_t s16_drive_rpmL_dlt_old;
-int16_t s16_drive_rpmL_dlt_sum;
+int16_t s16_drive_rpmL_err_old;
+int16_t s16_drive_rpmL_err_cumsum;
 
 int16_t s16_drive_rpmR_p;
 int16_t s16_drive_rpmR_i;
 int16_t s16_drive_rpmR_d;
-int16_t s16_drive_rpmR_dlt_old;
-int16_t s16_drive_rpmR_dlt_sum;
+int16_t s16_drive_rpmR_err_old;
+int16_t s16_drive_rpmR_err_cumsum;
 
 /*ロギング用にglobalで定義*/
 int16_t s16_drive_cntL;
 int16_t s16_drive_cntR;
 int16_t s16_drive_rpmL;
 int16_t s16_drive_rpmR;
-int16_t s16_drive_rpmL_dlt;
-int16_t s16_drive_rpmR_dlt;
-int16_t s16_drive_dltL;
-int16_t s16_drive_dltR;
-uint16_t u16_drive_cntL_buf_index;
-int16_t s16_drive_dltbufL;
+int16_t s16_drive_rpmL_err;
+int16_t s16_drive_rpmR_err;
+
 
 /* 駆動モータ初期化 */
 void ini_drive_mtr( void ){
@@ -77,23 +70,18 @@ void ini_drive_mtr( void ){
   pup_motor_reset_count(motorL);
   pup_motor_reset_count(motorR);
 
-  u16_drive_cntL_buf_index  = 0;
-  u16_drive_cntR_buf_index  = 0;
-  u16_drive_cntL_buf_re_flg = 0;
-  u16_drive_cntR_buf_re_flg = 0;
-
   g_s16_drive_mtr_spdL = 0;
   g_s16_drive_mtr_spdR = 0;
 
   s16_drive_rpmL_p = 0;
   s16_drive_rpmL_i = 0;
   s16_drive_rpmL_d = 0;
-  s16_drive_rpmL_dlt_old = 0;
+  s16_drive_rpmL_err_old = 0;
 
   s16_drive_rpmR_p = 0;
   s16_drive_rpmR_i = 0;
   s16_drive_rpmR_d = 0;
-  s16_drive_rpmR_dlt_old = 0;
+  s16_drive_rpmR_err_old = 0;
 }
 
 /* 駆動モータDUty設定 */
@@ -105,70 +93,51 @@ void set_drive_mtr_duty( int16_t dutyL, int16_t dutyR ){
 /* 駆動モータ回転速度設定 */
 void set_drive_mtr_spd( int16_t spdL, int16_t spdR ){
   
-  get_drive_mtr_cnt(&s16_drive_cntL,&s16_drive_cntR);
-  
-  if( MOTOR_CNT_BUF > u16_drive_cntL_buf_index ){
-    u16_drive_cntL_buf_index += 1;
-    if( 0 == u16_drive_cntL_buf_re_flg ){
-      s16_drive_cntL_buf[u16_drive_cntL_buf_index] = s16_drive_cntL;
-      s16_drive_cntR_buf[u16_drive_cntL_buf_index] = s16_drive_cntR;
-      return;
-    }
-  }
-  else{
-    u16_drive_cntL_buf_re_flg = 1;
-    u16_drive_cntL_buf_index = 0;
-  }
-  
-  s16_drive_dltbufL = s16_drive_cntL_buf[u16_drive_cntL_buf_index];
-  s16_drive_dltL    = s16_drive_cntL - s16_drive_cntL_buf[u16_drive_cntL_buf_index];
-  s16_drive_rpmL   = (int16_t)((int32_t)s16_drive_dltL *1000*60*6/ (360 * MOTOR_CNT_BUF * 2));
-  // s16_drive_rpmL   = (int16_t)((int32_t)s16_drive_dltL *1000*60/ (360 * MOTOR_CNT_BUF * 2));
-  //s16_drive_rpmL = pup_motor_get_speed(motorL);
-  s16_drive_rpmL_dlt = (spdL - s16_drive_rpmL);
-  s16_drive_rpmL_p = ( s16_drive_rpmL_dlt * x_u16_drive_mtr_rpmL_kp ) / 100;
-  s16_drive_rpmL_d = (( s16_drive_rpmL_dlt - s16_drive_rpmL_dlt_old ) * x_u16_drive_mtr_rpmL_kd ) / 100;
+  s16_drive_rpmL = pup_motor_get_speed(motorL);
+  s16_drive_rpmL_err = (spdL - s16_drive_rpmL);
+  s16_drive_rpmL_p = ( s16_drive_rpmL_err * x_u16_drive_mtr_rpmL_kp ) / 100;
+  s16_drive_rpmL_d = (( s16_drive_rpmL_err - s16_drive_rpmL_err_old ) * x_u16_drive_mtr_rpmL_kd ) / 100;
   if(s16_drive_rpmL_i <= 100)
   {
-    s16_drive_rpmL_dlt_sum += s16_drive_rpmL_dlt;  
+    s16_drive_rpmL_err_cumsum += s16_drive_rpmL_err;  
   }
-  s16_drive_rpmL_i = s16_drive_rpmL_dlt_sum*0.002 * x_u16_drive_mtr_rpmL_ki/100;
+  s16_drive_rpmL_i = s16_drive_rpmL_err_cumsum*0.002 * x_u16_drive_mtr_rpmL_ki/100;
   g_s16_drive_mtr_spdL = s16_drive_rpmL_p + s16_drive_rpmL_i - s16_drive_rpmL_d;
-  if( 100 <= g_s16_drive_mtr_spdL ){
+  if( 100 < g_s16_drive_mtr_spdL ){
     g_s16_drive_mtr_spdL = 100;
+    s16_drive_rpmL_err_cumsum = 0;
   }
-  else if( 0 >= g_s16_drive_mtr_spdL ){
-    g_s16_drive_mtr_spdL = 0;
+  else if( -100 > g_s16_drive_mtr_spdL ){
+    g_s16_drive_mtr_spdL = -100;
+    s16_drive_rpmL_err_cumsum = 0;
   }
-  //g_s16_drive_mtr_spdL += 50;
-  
-  s16_drive_cntL_buf[u16_drive_cntL_buf_index] = s16_drive_cntL;
-  s16_drive_rpmL_dlt_old = s16_drive_rpmL_dlt;
 
-  s16_drive_dltR  = s16_drive_cntR - s16_drive_cntR_buf[u16_drive_cntL_buf_index];  
-  //s16_drive_rpmR = (int16_t)((int32_t)s16_drive_dltR *1000*60/ (360 * MOTOR_CNT_BUF * 2));
   s16_drive_rpmR = pup_motor_get_speed(motorR);
-  s16_drive_rpmR_dlt = (spdR - s16_drive_rpmR);
-  s16_drive_rpmR_p = ( s16_drive_rpmR_dlt * x_u16_drive_mtr_rpmR_kp ) / 100;
-  s16_drive_rpmR_d = ((s16_drive_rpmR_dlt - s16_drive_rpmR_dlt_old) * x_u16_drive_mtr_rpmR_kd) / 100;
-  g_s16_drive_mtr_spdR = s16_drive_rpmR_p - s16_drive_rpmR_d;
-  if( 100 <= g_s16_drive_mtr_spdR ){
+  s16_drive_rpmR_err = (spdR - s16_drive_rpmR);
+  s16_drive_rpmR_p = ( s16_drive_rpmR_err * x_u16_drive_mtr_rpmR_kp ) / 100;
+  s16_drive_rpmR_d = ((s16_drive_rpmR_err - s16_drive_rpmR_err_old) * x_u16_drive_mtr_rpmR_kd) / 100;
+  if(s16_drive_rpmR_i <= 100)
+  {
+    s16_drive_rpmR_err_cumsum += s16_drive_rpmR_err;  
+  }
+  s16_drive_rpmR_i = s16_drive_rpmR_err_cumsum*0.002 * x_u16_drive_mtr_rpmR_ki/100;
+  g_s16_drive_mtr_spdR = s16_drive_rpmR_p +s16_drive_rpmR_i - s16_drive_rpmR_d;
+  if( 100 < g_s16_drive_mtr_spdR ){
     g_s16_drive_mtr_spdR = 100;
+    s16_drive_rpmR_err_cumsum = 0;
   }
-  else if( 0 >= g_s16_drive_mtr_spdR ){
-    g_s16_drive_mtr_spdR = 0;
+  else if( -100 > g_s16_drive_mtr_spdR ){
+    g_s16_drive_mtr_spdR = -100;
+    s16_drive_rpmR_err_cumsum = 0;
   }
-  //g_s16_drive_mtr_spdR += 50;
   
-  s16_drive_cntR_buf[u16_drive_cntL_buf_index] = s16_drive_cntR;
-  s16_drive_rpmR_dlt_old = s16_drive_rpmR_dlt;
+  s16_drive_rpmL_err_old = s16_drive_rpmL_err;
+  s16_drive_rpmR_err_old = s16_drive_rpmR_err;
 
-
-  //pup_motor_set_power(motorL, g_s16_drive_mtr_spdL);  // パワーを設定
+  pup_motor_set_power(motorL, g_s16_drive_mtr_spdL);  // パワーを設定 g_s16_drive_mtr_spdL
+  pup_motor_set_power(motorR, g_s16_drive_mtr_spdR);  // パワーを設定
   //pup_motor_set_speed(motorL, spdL);                  // スピードを設定
   //pup_motor_set_speed(motorR, spdR);                  // スピードを設定
-  pup_motor_set_power(motorL, 80);                  // パワーを設定
-  pup_motor_set_power(motorR, 80);                  // パワーを設定
 }
 
 /* 駆動モータエンコーダカウントリセット */
